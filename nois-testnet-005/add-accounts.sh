56d49914a340e6ebad2a51cff6f5dc9886aaeff0
thanhbinh <33974749+thanhbinhpc2007@users.noreply.github.com>
2023-03-29 22:48:47 +0700

    Update add-accounts.sh

--- nois-testnet-005/add-accounts.sh
+++ nois-testnet-005/add-accounts.sh
@@ -13,6 +13,7 @@ genesis_addresses=(
     "nois17ndyyhdtdx33c5nmnl40eamr9lg35ej7vacpuq"
     "nois1k343zj53k3w3ju58qmgug4kdzlhpf0tymj5r9t"
     "nois1wuyjnslsx64ehn5v8kr9d9wargc3vlea07aqm3"
+    "nois10gzkvkj5q23q5gpk47kftwjvxqhaerxfh2984s"
     # Add your address here in a PR
 
 )
